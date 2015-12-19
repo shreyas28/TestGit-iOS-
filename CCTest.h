aee34c63e5c3a25d27108694513765ec74255fb1
TejeshP <MAC@Shreyas-2.local>
2015-12-19 17:52:52 -0500

    added test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  TestGit
+//
+//  Created by Shreyas on 12/19/15.
+//  Copyright © 2015 SR5. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  TestGit
+//
+//  Created by Shreyas on 12/19/15.
+//  Copyright © 2015 SR5. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
